e2840b1e9fc0f07a7be808ae04db09d4d59fa81b
everhobbes <ever.hobbes@gmail.com>
2015-05-20 16:08:06 -0400

    menu, search, svg
    Htaccess update for svg support, search box styling. Search function
    fix.

--- wordpress/wp-content/themes/sofi/parts/top-bar.php
+++ wordpress/wp-content/themes/sofi/parts/top-bar.php
@@ -28,12 +28,8 @@
 							<a href="https://www.youtube.com/channel/UCRaxyvl99d3cK9Q4BvlCpUw" target="_blank"><img src="<?php echo get_bloginfo( 'stylesheet_directory' ); ?>/assets/img/icons/youtube_034.png" alt=""></a>
 						</li>
 						<li class="sb-search-li" id="sb-search-li">				 		
-							<!--form role="search" method="get" id="searchform" action="http://mastercardfdnsymposium.org/">
-								<input type="text" value="" name="s" id="s" placeholder="Search" />
-								<input type="submit" id="searchsubmit" value="" />
-							</form-->
 							<div id="sb-search" class="sb-search">
-								<form role="search" method="get" id="searchform" action="<?php get_site_url(); ?>">
+								<form role="search" method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
 									<input class="sb-search-input" placeholder="Search" value="" name="s" type="text">
 									<input class="sb-search-submit" id="searchsubmit" value="" type="submit">
 									<span class="sb-icon-search"><img src="<?php echo get_bloginfo( 'stylesheet_directory' ); ?>/assets/img/icons/search.png" alt=""></span>
